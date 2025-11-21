e2a0f43684d7cb5b109d345c68628bfca6b4abd3
2501101448-pixel <2501101448@student.buksu.edu.ph>
2025-11-21 15:10:51 +0800

    add lern more in the protect your data

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Back to Top Button
 const topBtn = document.getElementById("backToTop");
 
+// Show button when scrolling
+window.addEventListener("scroll", () => {
+  const btn = document.getElementById("backToTop");
+  if (window.scrollY > 200) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+});
+
+// Scroll to top smoothly
+document.getElementById("backToTop").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+
 // Search elements MUST be placed before they're used
 const searchInput = document.getElementById("heroSearchInput");
 const suggestionsList = document.getElementById("heroSearchSuggestions");
@@ -45,9 +64,13 @@ topBtn.addEventListener("click", function () {
 
 // List of topics with page URLs
 const topics = [
-  { name: "Protect Your Data", url: "page1.html" },
-  { name: "Recognize Online Scams", url: "page2.html" },
-  { name: "Cyberbullying Awareness", url: "page3.html" },
+  { name: "Protect Your Data", url: "blogpage1.html" },
+  { name: "Recognize Online Scams", url: "blogpage2.html" },
+  { name: "Cyberbullying Awareness", url: "blogpage3.html" },
+  { name: "Tips", url: "tips.html" },
+  { name: "Contact", url: "contact.html" },
+  { name: "about", url: "about.html" },
+  { name: "about", url: "aware.html" },
 ];
 
 // Show suggestions as user types
